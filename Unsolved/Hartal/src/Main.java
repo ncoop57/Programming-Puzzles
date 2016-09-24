ad9a6cf8466c68d8090f5a484f6cf0a13440a8e5
Nathan Cooper <nac33@students.uwf.edu>
2016-09-23 21:58:32 -0500

    Add solved and unsolved

---
+++ Unsolved/Hartal/src/Main.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] arg)
+	{
+		Scanner scan = new Scanner(System.in);
+		int days = 0;
+		int t;
+		
+		
+		t = scan.nextInt();
+		int[][] info = new int[t][3];
+		for(int i = 0; i < t; i++)
+			for(int j = 0; j < 3; j++)
+				info[i][j] = scan.nextInt();
+		
+		
+		for(int i = 0;)
+		{
+			
+		}
+		
+	}
+}
---
+++ Unsolved/pkg2/Main.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.pkg2;
+
+/**
+ *
+ * @author nac33
+ */
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        ArrayList<String> input = new ArrayList<String>();
+        Scanner scan = new Scanner(System.in);
+        
+        input.add(scan.nextLine());
+        input.add(scan.nextLine());
+        while(!(input.get(input.size() - 1).equals("0")))
+        {
+            input.add(scan.nextLine());
+        }
+        
+        input.remove(input.size() - 1);
+        int[] nums = new int[input.size() / 2];
+        String[] letters = new String[input.size() / 2];
+        for(int i = 0; i < input.size(); i++)
+        {
+            if(((i + 1) % 2) != 0)
+            {
+                nums[i / 2] = Integer.parseInt(input.get(i));
+            }
+            else letters[i / 2] = input.get(i);
+        }
+        
+        for(int j = 0; j < nums.length; j++)
+        {
+            char[][] sequence = new char[nums[j]][nums[j]];
+            
+            for(int k = 0; k < nums[j]; k++)
+            {
+                for(int l = 0; l < letters[j].length(); l++)
+                {
+                    if(k + 1 % 2 == 0)
+                    {
+                        sequence[k][l] = letters[j].charAt(l);
+                        System.out.println(sequence[k][l]);
+                    }
+                    else
+                    {
+                        sequence[k][l] = letters[j].charAt((letters[j].length() - 1) - l);
+                        System.out.println(sequence[k][l]);
+                    }
+                }
+            }
+        }
+        
+        
+    }
+    
+}
